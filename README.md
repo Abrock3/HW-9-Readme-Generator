395fae81318339c0956406f0c99bdbd18becbfba
Adam Brock <a.paulbrock@gmail.com>
2022-05-05 12:50:26 -0400

    Added readme. Refined function of index.js, and added more options for the user to use if they wish.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# HW-9-Readme-Generator
-This application will prompt a command line user with questions about their project and preferences and will generate a readme based on that information.
+# Readme Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+This app assists developers with the somewhat tedious task of writing a detailed readme. It functions by prompting the user to answer some prompts about their project, and when the series of prompts is complete the app will assemble a file named READMEsample.md using the provided information. I learned quite a bit while creating this project; most notably I firmed up my knowledge of inquirer and node; I also learned the basics of the markdown language.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation:
+
+Prior to use, the user must install node, and run "npm i" from the command line.
+
+## Usage
+
+Once the installation is complete, the user can run the application using "node index.js". Answer the prompts, and once it's complete look for READMEsample.md in your directory; this will be your assembled readme. Feel free to modify it after it's created if desired.
+
+Screenshot of the app during function:
+![Screenshot](assets\images\Screenshot-2022-05-03-234147.jpg?raw=true "Screenshot")
+
+## Credits
+
+I used this tutorial to learn how to replace all instances of a character in a string: https://www.tutorialrepublic.com/faq/how-to-replace-character-inside-a-string-in-javascript.php
+
+Some starter code was given to me by Trey Eckels, as this was an assignment given in the process of completing a full stack boot camp with Georgia Tech. It was simply empty functions, with sparse comments hinting at a general code structure.
+
+Node.js was used in the creation and implementation of this app: https://nodejs.org/en/
+
+Inquirer, a npm package, was used in the creation and implementation of this app: https://www.npmjs.com/package/inquirer?ref=hackernoon.com
+
+I have to credit my own app, for assisting me with creating this readme!
+
+## License
+
+Copyright 2022 Adam Brock
+
+This software is licensed using the MIT license: https://opensource.org/licenses/MIT.
+
+## Questions
+
+Feel free to reach out to me with questions at a.paulbrock@gmail.com.
+
+My GitHub profile is at https://github.com/abrock3.
--- index.js
+++ index.js
@@ -8,12 +8,24 @@ const questions = [
     message:
       "What is the name of your project? This will be displayed as the readme title.",
     name: "title",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme must have a title.";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     message:
       "Write a description of your project, including why you built it, what problem it solves, and optionally what you learned while making it.",
     name: "description",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme must have a description";
+      }
+      return true;
+    },
   },
   {
     type: "input",
@@ -24,8 +36,20 @@ const questions = [
   {
     type: "input",
     message:
+      "Paste a link to the deployed application. This will be placed in the usage section. You may omit this section by leaving this blank.",
+    name: "appLink",
+  },
+  {
+    type: "input",
+    message:
       "Write instructions on how to use the application, and provide examples. You'll be provided with an opportunity to embed a screenshot.",
     name: "usage",
+    validate(answer) {
+      if (!answer) {
+        return "Even if it appears intuitive, your readme must have usage instructions";
+      }
+      return true;
+    },
   },
   {
     type: "input",
@@ -50,6 +74,12 @@ const questions = [
     message:
       "Write a credits section and input it here. Be sure to include collaborators, with links to their GitHub profiles, as well as links to any third party assets and tutorials that you used to build this project.",
     name: "credits",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme must have a credits section";
+      }
+      return true;
+    },
   },
   {
     type: "list",
@@ -62,17 +92,34 @@ const questions = [
     message:
       "What's your first and last name? This will be used in the license section.",
     name: "devName",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme must include your name in the license section, in case your work is distributed";
+      }
+      return true;
+    },
   },
-
   {
     type: "input",
     message: "Input your email address for use in a 'questions' section.",
     name: "email",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme should include a way to contact you, even if it's an anonymous email";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     message: "Input your GitHub username for use in a 'questions' section.",
     name: "username",
+    validate(answer) {
+      if (!answer) {
+        return "Your readme should include your GitHub, in case your work is distributed";
+      }
+      return true;
+    },
   },
 ];
 
@@ -84,13 +131,13 @@ function init() {
 }
 
 function assembleReadme(readmeData) {
-  console.log(readmeData);
   let installationString = "";
   let screenshotString = "";
   let contributionString = "";
   let testingString = "";
   let licenseString = "";
   let licenseBadgeString = "";
+  let appLinkString = "";
   const date = new Date();
   const year = date.getFullYear();
   if (readmeData.installation) {
@@ -100,9 +147,21 @@ function assembleReadme(readmeData) {
 
 ${readmeData.installation}`;
   }
+  if (readmeData.appLink){
+    appLinkString = `
+    
+Link to the deployed app: ${readmeData.appLink}
+
+`;
+  }
   if (readmeData.screenshotPath) {
+    // Depending on how the user gets the relative filepath of the screenshot, it may have backslashes instead of forward slashes, which doesn't appear to work in markdown.
+    // This will replace all back slashes with forward slashes to make it function properly
+    let moddedScreenshotPath = readmeData.screenshotPath.replace(/"\\"/g, "/");
     screenshotString = `
-    ![Screenshot](${readmeData.screenshotPath}?raw=true "Screenshot")`;
+
+Screenshot of the app during function:
+![Screenshot](${moddedScreenshotPath}?raw=true "Screenshot")`;
   }
   if (readmeData.contribute) {
     contributionString = `
@@ -180,11 +239,14 @@ ${
       : ``
   }- [Credits](#credits)
 - [License](#license)
+- [Questions](#questions)
 ${installationString}
 
 ## Usage
 
-${readmeData.usage}${screenshotString}${contributionString}${testingString}
+${appLinkString}${
+    readmeData.usage
+  }${screenshotString}${contributionString}${testingString}
 
 ## Credits
 
@@ -193,6 +255,7 @@ ${readmeData.credits}${licenseString}
 ## Questions
 
 Feel free to reach out to me with questions at ${readmeData.email}.
+
 My GitHub profile is at https://github.com/${readmeData.username}.`;
   writeToFile(readmeText);
 }
